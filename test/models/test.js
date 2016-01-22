5aa35653cc956d716e73885a5aa64b8667758b30
EdDillinger <holger.pfaff@gmx.net>
2016-01-22 07:17:00 +0100

    Improved unit test for room-models

--- test/models/test.js
+++ test/models/test.js
@@ -1,16 +1,22 @@
 
 
-	var models1 = ['galleryboxV1.json', 'image1l.json', 'image2.json'];		
-	var rootPath1 = "../../app/models/room1/";
+	var models = [
+		{
+			files : ['galleryboxV1.json', 'image1l.json', 'image2.json'],		
+			path : "../../app/models/room1/"
+		},
+		{
+			files : ['galleryglassV1.json', 'imageboxV1.json'],		
+			path : "../../app/models/room2/"
+		},
+	],
+
 	
-	var models2 = ['galleryglassV1.json', 'imageboxV1.json'];		
-	var rootPath2 = "../../app/models/room2/";
-	
-	function isString(o) {
+	isString = function(o) {
 		return typeof o == "string" || (typeof o == "object" && o.constructor === String);
-	};
+	},
 	
-	var imageTest = function (url) 
+	imageTest = function (url) 
 	{		
 		try {
 			var http = new XMLHttpRequest();
@@ -22,9 +28,9 @@
 			console.log('Error when checking image: ' + e);
 			return false;
 		}
-	};
+	},
 
-	var findFile = function(val, callback) {
+	findFile = function(val, callback) {
 			
 		if(val === undefined || isString(val)) 
 			return;
@@ -41,10 +47,10 @@
 				}
 			}
 		};		
-	};
+	},
 	
 	
-	var checkTextureFiles = function(names, root, assert, doneCallback) {
+	checkTextureFiles = function(names, root, assert, doneCallback) {
 		var cnt = 0;
 		
 		names.forEach(function(m) {
@@ -78,14 +84,12 @@
 		
 	QUnit.start();
 	
-	QUnit.test("Check texture-files on json-models for room1", function(assert) {
-		var done = assert.async();
-		checkTextureFiles(models1, rootPath1, assert, function() { done(); });
+	QUnit.test("Check texture-files on json-models for all rooms", function(assert) {		
+		models.forEach(function(m) {
+			var done = assert.async();
+			console.log('Checking models in ' + m['path']);
+			checkTextureFiles(m['files'], m['path'], assert, function() { done(); });
+		});
 	});
 	
-	QUnit.test("Check texture-files on json-models for room2", function(assert) {
-		var done = assert.async();
-		checkTextureFiles(models2, rootPath2, assert, function() { done(); });
-	});
-	
-	
+
